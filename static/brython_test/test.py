6be7d66b6d3e22305405ae284fe010bf55b42a5a
Jeongmin Byun <jmbyun91@gmail.com>
2020-10-28 18:04:43 +0900

    remove brython source files and adjust the license and private

--- static/brython_test/test.py
+++
@@ -1,5 +0,0 @@
-print('This is a test python source.')
-
-print('Test importing...')
-
-import test_import
--- static/brython_test/test_import.py
+++
@@ -1,5 +0,0 @@
-
-print('Imported successfully.')
-print('Error is raised for the test, too.')
-
-b = a + 1
